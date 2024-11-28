60b86df2ab4e5eecb4e6228d0968f48eae27de49
fionntmcc <127796057+fionntmcc@users.noreply.github.com>
2024-11-28 11:16:24 +0000

    update Brows.js to get from server

--- story-reader/src/components/Browse.js
+++ story-reader/src/components/Browse.js
@@ -1,16 +1,17 @@
-// necessary inputs
+// necessary imports
 import React, { useEffect, useState } from "react";
 import Books from "./Books";
 import axios from "axios";
 import { useActionData } from "react-router-dom";
 
 // Reusable read component
-const Browse = () => {
+const Read = () => {
 
     // constant to store books as JSON
     const [books, setBooks] = useState([]); // initialise books to null array
 
     // useEffect() method to get books from API
+    /*
     useEffect(
         () => {
             axios.get("https://jsonblob.com/api/jsonblob/1311482858984103936") // gets from given API
@@ -25,10 +26,27 @@ const Browse = () => {
             )
         }, [] // only runs on init
     );
+    */
+
+    useEffect(
+        () => {
+            axios.get("http://localhost:4000/api/books") // gets from given API
+            .then((response) => {
+                console.log(response.data); // logs api response
+                setBooks(response.data.myBooks) // sets Books using useState()
+            })
+            .catch(
+                (error) => { // handle errors
+                    console.log(error); 
+                }
+            )
+        }, [] // only runs on init
+    );
     
     // return message and book list
     return <div>
             {/* display books */}
+            <h1>Hello from the Read component</h1>
             <Books myBooks={books} />
         </div>;
 }
