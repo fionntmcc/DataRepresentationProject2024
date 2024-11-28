5377a1c95e8375fb64d138d950c3843738f1f6fa
fionntmcc <127796057+fionntmcc@users.noreply.github.com>
2024-11-28 17:51:16 +0000

    connecting to bookdb

--- story-reader/src/backend/server.js
+++ story-reader/src/backend/server.js
@@ -1,24 +1,18 @@
-
-/*
-
 /*
     Express.js allows for easy creation of a server that 
     handles routes and uses URL params
 */
 const express = require('express');
-
 const app = express();
-
-const port = 4000; // port for website
-
-const cors = require('cors');
-app.use(cors());
-const Book = require('./models/Book');
+const port = 4000;
 
 /* 
     cors is a middleware that defines what a ips are allowed to communicate
     with the server. Protects against DOS attacks, etc.
 */
+const cors = require('cors');
+app.use(cors());
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
@@ -31,51 +25,25 @@ app.use(function(req, res, next) {
     BodyParser allows for us to parse the returned data easily,
     in json in this case
 */
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-
 const mongoose = require('mongoose');
-const MONGO_URI = 'mongodb+srv://admin:admin@merndb.3xcyk.mongodb.net/?retryWrites=true&w=majority&appName=MernDB';
+mongoose.connect('mongodb+srv://admin:admin@bookcluster.3xcyk.mongodb.net/bookdb');
 
-mongoose.connect(MONGO_URI)
-.then(() => {
-    console.log('Connected to Mongodb');
-}).catch((err) => {
-    console.error("Error connecting to MongoDB: ", err);
+const bookSchema = new mongoose.Schema({
+  title:String,
+  year:String,
+  poster:String
 });
 
-// Insert books into the database
-const books = [
-    { title: "The Catcher in the Rye", author: "J.D. Salinger", year: 1951 },
-    { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
-    { title: "1984", author: "George Orwell", year: 1949 },
-    { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
-    { title: "Pride and Prejudice", author: "Jane Austen", year: 1813 }
-  ];
-
-  
-
-  // Insert multiple documents (books) into the collection
-Book.insertMany(books)
-.then(() => {
-  console.log('Books inserted successfully');
-  mongoose.connection.close(); // Close the connection after insertion
-})
-.catch((err) => {
-  console.error('Error inserting books:', err);
-  mongoose.connection.close(); // Close the connection on error
-});
-
-app.get('/', (req, res) => {
-    res.send('API is running....'); 
-});
+const bookModel = new mongoose.model('mybooks',bookSchema);
 
 app.get('/api/books', async (req, res) => {
-    const books = await bookModel.find();
-    res.status(200).json({books});
+    const books = await bookModel.find({});
+    res.status(200).json({books})
 });
 
 app.get('/api/book/:id', async (req ,res)=>{
@@ -84,89 +52,40 @@ app.get('/api/book/:id', async (req ,res)=>{
 })
 
 app.post('/api/books',async (req, res)=>{
+    /*
+        The bodyParser middleware allows for access to the body of a post.
+        This is necessary because unlike the get method, data
+        is returned in the body, and not the URL.
+    */
     console.log(req.body.title);
-    const {Title, Tear, Poster} = req.body;
+    const {title, year, poster} = req.body;
 
     const newBook = new bookModel({title, year, poster});
     await newBook.save();
 
     res.status(201).json({"message":"Book Added!",Book:newBook});
-});
+})
 
-/*
-    The bodyParser allows for access to the body of a post.
-    This is necessary because unlike the get method, data
-    is returned in the body, and not the URL.
-*/
-
-const path = require('path');
-const { stringify } = require('querystring');
-
-//app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.static('public'));
-
-// return status code 500 if there is an error in gets / posts
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
-});
-
-/*
-// return books as JSON
-app.get("/api/books", (req, res) => {
-    // app returns back JSON on books
-    const books = [
-        {
-            "Title": "The Great Gatsby",
-            "Author": "F. Scott Fitzgerald",
-            "id": "1",
-            "isFavorite": false,
-            "Year": 1925,
-            "Genre": "Fiction",
-            "Poster": "https://th.bing.com/th/id/OIP.fFaX7nKq5_5gf2nSI3QEUgHaLK?w=186&h=280&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-            "Summary": "The story of the mysteriously wealthy Jay Gatsby and his love for the beautiful Daisy Buchanan, of lavish parties on Long Island at a time when The New York Times noted 'gin was the national drink",
-            "Rating": 4.5,
-            "Text": "https://www.gutenberg.org/files/64317/64317-0.txt"
-        },
-        {
-            "Title": "The Catcher in the Rye",
-            "Author": "J.D. Salinger",
-            "id": "2",
-            "isFavorite": false,
-            "Year": 1951,
-            "Genre": "Fiction",
-            "Poster": "https://th.bing.com/th?id=OIP.ZesfpsZfOMGuCiE2Rn5m6QHaLH&w=204&h=306&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2",
-            "Summary": "The story of Holden Caulfield, a teenager at a prep school in New York City, and his many encounters with friends and strangers",
-            "Rating": 4,
-            "Text": "https://www.gutenberg.org/files/507/507-0.txt"
-        },
-        {
-            "Title": "To Kill a Mockingbird",
-            "Author": "Harper Lee",
-            "id": "3",
-            "isFavorite": false,
-            "Year": 1960,
-            "Genre": "Fiction",
-            "Poster": "https://th.bing.com/th/id/OIP.jvYLjJvwkKBnI2svjjyKigHaLI?w=186&h=280&c=7&r=0&o=5&dpr=1.3&pid=1.7",
-            "Rating": 4.5,
-            "Text": "https://www.gutenberg.org/files/437/437-0.txt"
-        }
-    ];
-
-
-      // post response to client about added book
-      app.post("/api/books", (req, res) => {
-        res.send("Book added!")
-      });
-
-    // status ok
-    res.status(200).json({ myBooks: books });
-
-});
-*/
-
-// log port to console
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
+});
+
+// Fetches the specific book's info.
+// Takes updated details from req.body 
+// and updates the book in the DB.
+// returns updated book to confirm the change.
+
+app.get('/api/book/:id', async (req, res) => {
+  let book = await bookModel.findById({ _id: req.params.id });
+  res.send(book);
+});
+
+// Updates the  specific book's info.
+// User submits the edited data.
+// Route takes the updated details from req.body.
+// Updates the book in the DB.
+// Returns updated book to confirm the change
+app.put('/api/book/:id', async (req, res) => {
+  let book = await bookModel.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.send(book);
 });
